f00e6df8e73ce5aa2b2e1342e99c0313cb5a9eb5
TuanNotFound <143462930+tuannotfound@users.noreply.github.com>
2025-03-29 13:58:05 +0700

    update Content

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-console.log("Hello World")
-    function iLoveU(heart) {
-      if (heart) {
-        return true
-      } else {
-        return false
-      }
-    };
+//console.log("Hello World")
+function youreGay() {
+  document.write("You're Gay");
+};
+function iLoveU(heart) {
+  if (heart) {
+    return true
+  } else {
+    return false
+  };
+};
+
+let soKH = document.getElementById("btw");
+let saveNum = document.getElementById("save-num")
+let soHanhKhach = 0;
+function tang() {
+  soHanhKhach += 1;
+  soKH.textContent = soHanhKhach;
+}
+function save() {
+  saveNum.textContent += " " + soHanhKhach
+  soKH.textContent = 0;
+  soHanhKhach = 0;
+}
